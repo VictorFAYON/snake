13de229144745cb6017621c1718a915f94c906e8
Victor Fayon <victor.fayon@et.minesparis.psl.eu>
2024-11-05 16:52:59 +0100

    commmit branch session1

--- snake/main.py
+++ snake/main.py
@@ -1,2 +1,9 @@
 def ouf():
     print("ouf !")
+import argparse
+
+def h():
+    parser = argparse.ArgumentParser(description='Some description.')
+    parser.add_argument('-a', help="A text explaining what is the use of the -a option and what type of value it takes.")
+    args = parser.parse_args()
+    print(args)
